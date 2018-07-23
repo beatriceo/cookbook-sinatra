e6c90520e3888d95e029e1a25f3dd63697feea0f
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-07-23 16:23:07 +0100

    created basic template for adding recipes

--- app.rb
+++ app.rb
@@ -7,9 +7,25 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do # <- Router part
+get '/new' do # <- Router part
   # [...]  #
   # [...]  # <- Controller part
   # [...]  #
-  'test'
+  #
+  erb :recipe_form
 end
+
+get '/' do
+  @recipes = [ 'replace', 'with', 'recipe', 'objects' ]
+  erb :index
+end
+
+post '/recipes' do
+  params.to_s # access parameters and create a new recipe object
+  redirect '/'
+end
+
+# get '/team/:username' do
+#   puts params[:username]
+#   "The username is #{params[:username]}"
+# end
